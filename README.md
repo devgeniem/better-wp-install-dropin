1c5dd30e8bb28ec701784d156f25e2ecd496d452
carstenbach <mail@carsten-bach.de>
2023-02-01 12:12:29 +0100

    Initial refactor

--- README.md
+++ README.md
@@ -1,17 +1,43 @@
-![geniem-github-banner](https://cloud.githubusercontent.com/assets/5691777/14319886/9ae46166-fc1b-11e5-9630-d60aa3dc4f9e.png)
-# WP Dropin: Better install.php
-[![Build Status](https://travis-ci.org/devgeniem/better-wp-install-dropin.svg?branch=master)](https://travis-ci.org/devgeniem/better-wp-install-dropin) [![Latest Stable Version](https://poser.pugx.org/devgeniem/better-wp-install-dropin/v/stable)](https://packagist.org/packages/devgeniem/better-wp-install-dropin) [![Total Downloads](https://poser.pugx.org/devgeniem/better-wp-install-dropin/downloads)](https://packagist.org/packages/devgeniem/better-wp-install-dropin) [![Latest Unstable Version](https://poser.pugx.org/devgeniem/better-wp-install-dropin/v/unstable)](https://packagist.org/packages/devgeniem/better-wp-install-dropin) [![License](https://poser.pugx.org/devgeniem/better-wp-install-dropin/license)](https://packagist.org/packages/devgeniem/better-wp-install-dropin)
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/figuren-theater/install.php">
+    <img src="https://raw.githubusercontent.com/figuren-theater/logos/main/favicon.png" alt="figuren.theater Logo" width="100" height="100">
+  </a>
+
+  <h1 align="center">figuren.theater | install.php</h1>
+
+  <p align="center">
+    WordPress installer-dropin for the <a href="https://figuren.theater">figuren.theater</a> Multisite network.
+    <br /><br /><br />
+    <a href="https://meta.figuren.theater/blog"><strong>Read our blog</strong></a>
+    <br />
+    <br />
+    <a href="https://figuren.theater">See the network in action</a>
+    •
+    <a href="https://mein.figuren.theater">Join the network</a>
+    •
+    <a href="https://websites.fuer.figuren.theater">Create your own network</a>
+  </p>
+</div>
+
+## About 
+
+Forked from [devgeniem/better-wp-install-dropin: Slightly modified WordPress installer.php dropin for cleaner content after installation.](https://github.com/devgeniem/better-wp-install-dropin)
+
+---
 
 This `install.php` dropin doesn't install bloat from default install.php and sets a few opionated wp options.
 
 It doesn't send you email after installing new WordPress either.
 
 ## Current options
-- Set empty page as Front page
+- ~~Set empty page as Front page~~
 - Don't use any widgets
 - Use empty blog description ( if someone forgots to change that )
 - Use `/%category%/%postname%/` permalink
-- Don't install any articles
+- ~~Don't install any articles~~
+- Use the `ft_install_defaults` hook to add custom functionality
 
 ## Installation
 You can copy `install.php` to your `wp-content` folder. Just plug&play.
@@ -20,7 +46,7 @@ OR you can use composer so that you can automatically update it too. Put these i
 ```json
 {
     "require": {
-        "devgeniem/better-wp-install-dropin": "^1.0"
+        "figuren-theater/install.php": "^1.0"
     },
     "extra": {
         "dropin-paths": {
@@ -31,9 +57,11 @@ OR you can use composer so that you can automatically update it too. Put these i
 ```
 
 ## License
-GPLv3
 
-## Maintainers
-[@onnimonni](https://github.com/onnimonni)
+This project is licensed under the [GPL-3.0-or-later](LICENSE.md), see the [LICENSE](LICENSE) file for
+details
 
-[@Nomafin](https://github.com/Nomafin)
+## Acknowledgments to the original maintainers
+
+- [@onnimonni](https://github.com/onnimonni)
+- [@Nomafin](https://github.com/Nomafin)
--- install.php
+++ install.php
@@ -1,14 +1,11 @@
 <?php
 /**
- * Plugin name: Better WP install.php dropin
- * Plugin URI: https://github.com/devgeniem/better-wp-install-dropin
- * Description: This dropin doesn't install bloat content like the default install.php and sets a few opionated wp options.
- * Author: Onni Hakala / Geniem Oy
- * Author URI: https://github.com/onnimonni
- * License: GPLv3
- * License URI: https://opensource.org/licenses/GPL-3.0
- * Version: 1.0
+ * 
  */
+
+add_action( 'ft_install_defaults', 'ft_install_defaults__set_https_urls' );
+add_action( 'ft_install_defaults', 'ft_install_defaults__category' );
+add_action( 'ft_install_defaults', 'ft_install_defaults__post' );
 
 /**
  * Installs the site.
@@ -27,90 +24,108 @@
  * @param string $language      Optional. Language chosen. Default empty.
  * @return array Array keys 'url', 'user_id', 'password', and 'password_message'.
  */
-function wp_install( string $blog_title, string $user_name, string $user_email, bool $public,
-    string $deprecated = '', string $user_password = '', string $language = '' ) {
+function wp_install( string $blog_title, string $user_name, string $user_email, bool $public, string $deprecated = '', string $user_password = '', string $language = '' ) {
 
-  if ( ! empty( $deprecated ) ) {
-    _deprecated_argument( __FUNCTION__, '2.6' );
-  }
+	if ( ! empty( $deprecated ) ) {
+		_deprecated_argument( __FUNCTION__, '2.6' );
+	}
 
-  wp_check_mysql_version();
-  wp_cache_flush();
-  make_db_current_silent();
-  populate_options();
-  populate_roles();
+	wp_check_mysql_version();
+	wp_cache_flush();
+	make_db_current_silent();
+	populate_options();
+	populate_roles();
 
-  // Use language from installer or env WPLANG or default to 'fi'.
-  if ( ! empty( $language ) ) {
-    update_option( 'WPLANG', $language );
-  } elseif ( ! empty( getenv( 'WPLANG' ) ) ) {
-    update_option( 'WPLANG', getenv( 'WPLANG' ) );
-  } else {
-    update_option( 'WPLANG', 'fi' );
-  }
+	update_option( 'blogname', $blog_title );
+	update_option( 'admin_email', $user_email );
+	update_option( 'blog_public', $public );
+	// Prefer empty description if someone forgots to change it.
+	update_option( 'blogdescription', '' );
 
-  update_option( 'blogname', $blog_title );
-  update_option( 'admin_email', $user_email );
-  update_option( 'blog_public', $public );
+	// Freshness of site - in the future, this could get more specific about actions taken, perhaps.
+	update_option( 'fresh_site', 1 );
 
-  // Prefer empty description if someone forgots to change it.
-  update_option( 'blogdescription', '' );
+	// Use language from installer or env WPLANG or default to 'de_DE'.
+	if ( ! empty( $language ) ) {
+		update_option( 'WPLANG', $language );
+	} elseif ( ! empty( getenv( 'WPLANG' ) ) ) {
+		update_option( 'WPLANG', getenv( 'WPLANG' ) );
+	} else {
+		update_option( 'WPLANG', 'de_DE' );
+	}
 
-  $guessurl = wp_guess_url();
+	$guessurl = wp_guess_url();
 
-  update_option( 'siteurl', $guessurl );
+	update_option( 'siteurl', $guessurl );
 
-  // If not a public blog, don't ping.
-  if ( ! $public ) {
-    update_option( 'default_pingback_flag', 0 );
-  }
+	// If not a public blog, don't ping.
+	if ( ! $public ) {
+		update_option( 'default_pingback_flag', 0 );
+	}
 
-  /*
-   * Create default user. If the user already exists, the user tables are
-   * being shared among blogs. Just set the role in that case.
-   */
-  $user_id = username_exists( $user_name );
-  $user_password = trim( $user_password );
-  $email_password = false;
-  if ( ! $user_id && empty( $user_password ) ) {
-    $user_password = wp_generate_password( 12, false );
-    $message = __('<strong><em>Note that password</em></strong> carefully! It is a <em>random</em> password that was generated just for you.');
-    $user_id = wp_create_user($user_name, $user_password, $user_email);
-    update_user_option($user_id, 'default_password_nag', true, true);
-    $email_password = true;
-  } elseif ( ! $user_id ) {
-    // Password has been provided.
-    $message = '<em>'.__('Your chosen password.').'</em>';
-    $user_id = wp_create_user($user_name, $user_password, $user_email);
-  } else {
-    $message = __('User already exists. Password inherited.');
-  }
+	/*
+	* Create default user. If the user already exists, the user tables are
+	* being shared among blogs. Just set the role in that case.
+	*/
+	$user_id        = username_exists( $user_name );
+	$user_password  = trim( $user_password );
+	$email_password = false;
+	$user_created   = false;
 
-  $user = new WP_User($user_id);
-  $user->set_role('administrator');
+	if ( ! $user_id && empty( $user_password ) ) {
+		$user_password = wp_generate_password( 12, false );
+		$message       = __( '<strong><em>Note that password</em></strong> carefully! It is a <em>random</em> password that was generated just for you.' );
+		$user_id       = wp_create_user( $user_name, $user_password, $user_email );
+		update_user_meta( $user_id, 'default_password_nag', true );
+		$email_password = true;
+		$user_created   = true;
+	} elseif ( ! $user_id ) {
+		// Password has been provided.
+		$message      = '<em>' . __( 'Your chosen password.' ) . '</em>';
+		$user_id      = wp_create_user( $user_name, $user_password, $user_email );
+		$user_created = true;
+	} else {
+		$message = __( 'User already exists. Password inherited.' );
+	}
 
-  wp_install_defaults($user_id);
+	$user = new WP_User( $user_id );
+	$user->set_role( 'administrator' );
 
-  wp_install_maybe_enable_pretty_permalinks();
+	if ( $user_created ) {
+		$user->user_url = $guessurl;
+		wp_update_user( $user );
+	}
 
-  flush_rewrite_rules();
+	wp_install_defaults( $user_id );
 
-  wp_cache_flush();
+	wp_install_maybe_enable_pretty_permalinks();
 
-  /**
-   * Fires after a site is fully installed.
-   *
-   * @since 3.9.0
-   *
-   * @param WP_User $user The site owner.
-   */
-  do_action( 'wp_install', $user );
+	flush_rewrite_rules();
 
-  return array( 'url' => $guessurl, 'user_id' => $user_id, 'password' => $user_password, 'password_message' => $message );
+	// no wp_new_blog_notification() here, like normaly
+
+	wp_cache_flush();
+
+	/**
+	* Fires after a site is fully installed.
+	*
+	* @since 3.9.0
+	*
+	* @param WP_User $user The site owner.
+	*/
+	do_action( 'wp_install', $user );
+
+	return [
+		'url'              => $guessurl,
+		'user_id'          => $user_id,
+		'password'         => $user_password,
+		'password_message' => $message,
+	];
 }
 
 /**
  * Creates the initial content for a newly-installed site.
+ * switch_to_blog ran directly before this is called, so we're already in the right context.
  *
  * Adds the default "Uncategorized" category, the first post (with comment),
  * first page, and default widgets for default theme for the current version.
@@ -120,118 +135,267 @@ function wp_install( string $blog_title, string $user_name, string $user_email, 
  * @param int $user_id User ID.
  */
 function wp_install_defaults( int $user_id ) {
-  global $wpdb, $wp_rewrite, $current_site, $table_prefix;
+	global $wpdb, $wp_rewrite, $current_site, $table_prefix;
 
-  /**
-   * Time zone: Get the one from TZ environmental variable
-   *
-   * @see wp-admin/options-general.php
-   */
-  update_option( 'timezone_string', ( ! empty( getenv( 'TZ' ) ) ? getenv( 'TZ' ) : 'Europe/Helsinki' ) );
+	/**
+	* Time zone: Get the one from TZ environmental variable
+	*
+	* @see wp-admin/options-general.php
+	*/
+	update_option( 'timezone_string', ( ! empty( getenv( 'TZ' ) ) ? getenv( 'TZ' ) : 'Europe/Berlin' ) );
 
-  /**
-   * We don't want any default widgets. This fixes 'Undefined index: wp_inactive_widgets'
-   *
-   * @see wp-includes/widgets.php:1208
-   */
-  update_option( 'sidebars_widgets', array( 'wp_inactive_widgets' => array() ) );
+	/**
+	* We don't want any default widgets. This fixes 'Undefined index: wp_inactive_widgets'
+	*
+	* @see wp-includes/widgets.php:1208
+	*/
+	update_option( 'sidebars_widgets', [ 'wp_inactive_widgets' => [] ] );
 
-  /**
-   * Before a comment appears a comment must be manually approved: true
-   *
-   * @see wp-admin/options-discussion.php
-   */
-  update_option( 'comment_moderation', 1 );
+	/**
+	* Before a comment appears a comment must be manually approved: true
+	*
+	* @see wp-admin/options-discussion.php
+	*/
+	update_option( 'comment_moderation', 1 );
 
-  /** Before a comment appears the comment author must have a previously approved comment: false */
-  update_option( 'comment_whitelist', 0 );
+	/** Before a comment appears the comment author must have a previously approved comment: false */
+	update_option( 'comment_whitelist', 0 );
 
-  /** Allow people to post comments on new articles (this setting may be overridden for individual articles): false */
-  update_option( 'default_comment_status', 0 );
+	/** Allow people to post comments on new articles (this setting may be overridden for individual articles): false */
+	update_option( 'default_comment_status', 0 );
 
-  /** Allow link notifications from other blogs: false */
-  update_option( 'default_ping_status', 0 );
+	/** Allow link notifications from other blogs: false */
+	update_option( 'default_ping_status', 0 );
 
-  /** Attempt to notify any blogs linked to from the article: false */
-  update_option( 'default_pingback_flag', 0 );
+	/** Attempt to notify any blogs linked to from the article: false */
+	update_option( 'default_pingback_flag', 0 );
 
-  /**
-   * Organize my uploads into month- and year-based folders: true
-   *
-   * @see wp-admin/options-media.php
-   */
-  update_option( 'uploads_use_yearmonth_folders', 1 );
+	/**
+	* Organize my uploads into month- and year-based folders: true
+	*
+	* @see wp-admin/options-media.php
+	*/
+	update_option( 'uploads_use_yearmonth_folders', 1 );
 
-  /**
-   * Permalink custom structure: /%category%/%postname%
-   *
-   * @see wp-admin/options-permalink.php
-   */
-  update_option( 'permalink_structure', '/%category%/%postname%/' );
+	/**
+	* Permalink custom structure: /%category%/%postname%
+	*
+	* @see wp-admin/options-permalink.php
+	*/
+	update_option( 'permalink_structure', '/%category%/%year%/%monthnum%/%postname%/' );
+//////
+//  //
+//////
 
-  /**
-   * Create Default category.
-   */
-  // Somehow translations won't work always. So check if Finnish was used.
-  $cat_name = ( get_option('WPLANG') == 'fi' ) ? 'Yleinen' : __('Uncategorized');
 
-  /* translators: Default category slug */
-  $cat_slug = sanitize_title( ( get_option('WPLANG') == 'fi' ) ? 'yleinen' : _x('Uncategorized', 'Default category slug') );
+	//
+	do_action( 'ft_install_defaults', $user_id );
 
-  if ( global_terms_enabled() ) {
-    $cat_id = $wpdb->get_var( $wpdb->prepare( "SELECT cat_ID FROM {$wpdb->sitecategories} WHERE category_nicename = %s", $cat_slug ) );
-    if ( null == $cat_id ) {
-      $wpdb->insert( $wpdb->sitecategories, array( 'cat_ID' => 0, 'cat_name' => $cat_name, 'category_nicename' => $cat_slug, 'last_updated' => current_time('mysql', true) ) );
-      $cat_id = $wpdb->insert_id;
-    }
-    update_option('default_category', $cat_id);
-  } else {
-    $cat_id = 1;
-  }
 
-  $wpdb->insert( $wpdb->terms, array( 'term_id' => $cat_id, 'name' => $cat_name, 'slug' => $cat_slug, 'term_group' => 0 ) );
-  $wpdb->insert( $wpdb->term_taxonomy, array( 'term_id' => $cat_id, 'taxonomy' => 'category', 'description' => '', 'parent' => 0, 'count' => 1 ) );
-  $cat_tt_id = $wpdb->insert_id;
+	/**
+	* Create new page and add it as front page
+	$id = wp_insert_post( array(
+		// Somehow translations won't work always. So check if Finnish was used.
+		'post_title' => ( get_option('WPLANG') == 'fi' ) ? 'Etusivu' : __('Front page'),
+		'post_type' => 'page',
+		'post_status' => 'publish',
+		// Prefer empty content if someone forgots to change it.
+		'post_content' => ''
+	) );
 
-  /**
-   * Create new page and add it as front page
-   */
-  $id = wp_insert_post( array(
-    // Somehow translations won't work always. So check if Finnish was used.
-    'post_title' => ( get_option('WPLANG') == 'fi' ) ? 'Etusivu' : __('Front page'),
-    'post_type' => 'page',
-    'post_status' => 'publish',
-    // Prefer empty content if someone forgots to change it.
-    'post_content' => ''
-  ) );
+	// Add page we just created as front page
+	update_option( 'page_on_front' , $id );
+	update_option( 'show_on_front' , 'page' );
+	*/
 
-  // Add page we just created as front page
-  update_option( 'page_on_front' , $id );
-  update_option( 'show_on_front' , 'page' );
 
-  if ( is_multisite() ) {
-    // Flush rules to pick up the new page.
-    $wp_rewrite->init();
-    $wp_rewrite->flush_rules();
+	if ( ! is_super_admin( $user_id ) && ! metadata_exists( 'user', $user_id, 'show_welcome_panel' ) ) {
+		update_user_meta( $user_id, 'show_welcome_panel', 2 );
+	}
 
-    $user = new WP_User($user_id);
-    $wpdb->update( $wpdb->options, array( 'option_value' => $user->user_email ), array( 'option_name' => 'admin_email' ) );
+	// if ( is_multisite() ) {
+		// Flush rules to pick up the new page.
+		$wp_rewrite->init();
+		$wp_rewrite->flush_rules();
 
-    // Remove all perms except for the login user.
-    $wpdb->query( $wpdb->prepare("DELETE FROM $wpdb->usermeta WHERE user_id != %d AND meta_key = %s", $user_id, $table_prefix.'user_level') );
-    $wpdb->query( $wpdb->prepare("DELETE FROM $wpdb->usermeta WHERE user_id != %d AND meta_key = %s", $user_id, $table_prefix.'capabilities') );
+		$user = new WP_User( $user_id );
+		$wpdb->update( $wpdb->options, array( 'option_value' => $user->user_email ), array( 'option_name' => 'admin_email' ) );
 
-    // Delete any caps that snuck into the previously active blog. (Hardcoded to blog 1 for now.) TODO: Get previous_blog_id.
-    if ( ! is_super_admin( $user_id ) && 1 != $user_id ) {
-      $wpdb->delete( $wpdb->usermeta, array( 'user_id' => $user_id, 'meta_key' => $wpdb->base_prefix.'1_capabilities' ) );
-    }
-  }
+		// Remove all perms except for the login user.
+		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->usermeta WHERE user_id != %d AND meta_key = %s", $user_id, $table_prefix . 'user_level' ) );
+		$wpdb->query( $wpdb->prepare( "DELETE FROM $wpdb->usermeta WHERE user_id != %d AND meta_key = %s", $user_id, $table_prefix . 'capabilities' ) );
 
-  /**
-   * Show welcome panel: false
-   *
-   * @see wp-admin/includes/screen.php
-   */
-  update_user_meta( $user_id, 'show_welcome_panel', 0 );
+		// Delete any caps that snuck into the previously active blog. (Hardcoded to blog 1 for now.)
+		// TODO: Get previous_blog_id.
+		if ( ! is_super_admin( $user_id ) && 1 != $user_id ) {
+			$wpdb->delete(
+				$wpdb->usermeta,
+				array(
+					'user_id'  => $user_id,
+					'meta_key' => $wpdb->base_prefix . '1_capabilities',
+				)
+			);
+		}
+	// }
+
 }
 
+
+/**
+ * Blocking WordPress from sending installation email notice
+ *
+ * Empty function
+function wp_new_blog_notification( $blog_title, $blog_url, $user_id, $password ) { 
+/* empty function * /
+ }
+ */
+
+
+function ft_install_defaults__post( int $user_id ) : void {
+	
+	// First post.
+	$now             = current_time( 'mysql' );
+	$now_gmt         = current_time( 'mysql', 1 );
+	$first_post_guid = get_option( 'home' ) . '/?p=1';
+
+	$first_post = get_site_option( 'first_post' );
+
+	if ( ! $first_post ) {
+		$first_post = "<!-- wp:paragraph -->\n<p>" .
+		/* translators: First post content. %s: Site link. */
+		__( 'Welcome to %s. This is your first post. Edit or delete it, then start writing!' ) .
+		"</p>\n<!-- /wp:paragraph -->";
+	}
+
+	$first_post = sprintf(
+		$first_post,
+		sprintf( 
+			'<a href="%s">%s</a>', 
+			esc_url( network_home_url() ), 
+			get_network()->site_name 
+		)
+	);
+
+	$wpdb->insert(
+		$wpdb->posts,
+		array(
+			'post_author'           => $user_id,
+			'post_date'             => $now,
+			'post_date_gmt'         => $now_gmt,
+			'post_content'          => $first_post,
+			'post_excerpt'          => '',
+			'post_title'            => __( 'Hallo Theaterwelt!' ),
+			/* translators: Default post slug. */
+			'post_name'             => sanitize_title( _x( 'Hallo Theaterwelt', 'Default post slug' ) ),
+			'post_modified'         => $now,
+			'post_modified_gmt'     => $now_gmt,
+			'guid'                  => $first_post_guid,
+			'comment_count'         => 1,
+			'to_ping'               => '',
+			'pinged'                => '',
+			'post_content_filtered' => '',
+		)
+	);
+
+	if ( is_multisite() ) {
+		update_posts_count();
+	}
+
+	$wpdb->insert(
+		$wpdb->term_relationships,
+		array(
+			'term_taxonomy_id' => $cat_tt_id,
+			'object_id'        => 1,
+		)
+	);
+
+}
+
+
+function ft_install_defaults__category( int $user_id ) : void {
+	/**
+	* Create Default category.
+	*/
+	$cat_name = __( 'Uncategorized' );
+	/* translators: Default category slug. */
+	$cat_slug = sanitize_title( _x( 'Uncategorized', 'Default category slug' ) );
+
+	$cat_id = 1;
+
+	$wpdb->insert(
+		$wpdb->terms,
+		array(
+			'term_id'    => $cat_id,
+			'name'       => $cat_name,
+			'slug'       => $cat_slug,
+			'term_group' => 0,
+		)
+	);
+	$wpdb->insert(
+		$wpdb->term_taxonomy,
+		array(
+			'term_id'     => $cat_id,
+			'taxonomy'    => 'category',
+			'description' => '',
+			'parent'      => 0,
+			'count'       => 1,
+		)
+	);
+	$cat_tt_id = $wpdb->insert_id;
+}
+
+
+/**
+ * Plugin auto-activation function
+ *
+ * https://gist.github.com/brasofilo/4242948#file-install-php-L16
+ * http://wordpress.stackexchange.com/q/4041/12615
+function wpse_4041_run_activate_plugin( $plugin )
+{
+    $current = get_option( 'active_plugins' );
+    $plugin  = plugin_basename( trim( $plugin ) );
+
+    if( !in_array( $plugin, $current ) )
+    {
+        $current[] = $plugin;
+        sort( $current );
+        do_action( 'activate_plugin', trim( $plugin ) );
+        update_option( 'active_plugins', $current );
+        do_action( 'activate_' . trim( $plugin ) );
+        do_action( 'activated_plugin', trim( $plugin ) );
+    }
+
+    return null;
+}
+    // Activate our plugins
+    // wpse_4041_run_activate_plugin( 'akismet/akismet.php' );
+ */
+
+
+/**
+ * Set important URLs to HTTPS
+ * what is not done by default. 
+ * 
+ * The normal install_routine prevents 'https' explicitly for subdomain-installs
+ *
+ * @see      https://github.com/WordPress/WordPress/blob/ba9dd1d7d7dd84eabef6962e07e50e83763f1e8b/wp-includes/ms-site.php#L696
+ *
+ * @subpackage Figuren_Theater\Network\Setup
+ * @version    2022-10-05
+ * @author     Carsten Bach
+ *
+ */
+function ft_install_defaults__set_https_urls() {
+	$_options_to_change = [
+		'home',
+		'siteurl',
+	];
+	array_map(function( $option ){
+		$_updated_option = str_replace('http://', 'https://', get_option( $option ) );
+
+		update_option( 
+			$option,
+			$_updated_option
+		);
+	}, $_options_to_change);
+}
